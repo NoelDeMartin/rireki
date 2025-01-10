61a42a20d31df33a0c5bfd515c5ec9307729936b
Noel De Martin <noel@noeldemartin.com>
2025-01-10 18:59:40 +0100

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rireki',
-    version='0.1.1',
+    version='0.1.2',
     author='Noel De Martin',
     author_email='noeldemartin@gmail.com',
     description='CLI backup tool.',
